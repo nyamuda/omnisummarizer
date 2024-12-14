5a7d69fcda07906990058a9910add47337c9d39f
ptnrlab@gmail.com <ptnrlab@gmail.com>
2024-12-14 03:59:19 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,11 +1,15 @@
 import anvil.secrets
 import anvil.server
 from huggingface_hub import InferenceClient
+from huggingface_hub.inference_api import InferenceApi
 import re
 API_TOKEN=anvil.secrets.get_secret('HUGGINGFACE_API_TOKEN')
-headers = {"max_length": 50}
 client = InferenceClient("nyamuda/extractive-summarization",
                          token=API_TOKEN)
+
+# Initialize the Inference API
+inference = InferenceApi(
+    repo_id="nyamuda/extractive-summarization", token=API_TOKEN, task="summarization")
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
@@ -33,10 +37,45 @@ def summarize_text(text):
   #clean summary
   summary=clean_summary(response["summary_text"])
   return summary
-
+#summarize using the inference API
+#this allows to set the max and min summary length
+@anvil.server.callable
+def summarize_with_length(prompt,text, max_length,min_length):
+  parameters = {
+        "max_length": max_length,
+        "min_length": min_length,
+        "num_beams": 4,  # Optional: adjust for better generation quality
+        "length_penalty": 2.0  # Optional: adjust penalty to balance length
+    }
+  response = inference(f"{prompt}: {text}", params=parameters)
+  # Get the summarized text
+  summaryText = response[0]["summary_text"]
+  return summaryText
+  
 
 @anvil.server.route("/summarize")
 def summary_request():
+  prompt = anvil.server.request.body_json["prompt"]
   text = anvil.server.request.body_json["text"]
-  response=summarize_text(text)
-  return {"summary":response}
+  max_length = anvil.server.request.body_json["max_length"]
+  min_length = anvil.server.request.body_json["min_length"]
+
+  summaryText=summarize_with_length(prompt,text,max_length,min_length)
+  
+  return {"summary":summaryText}
+
+@anvil.server.callable
+def test_code():
+    text="Johannes Gutenberg (1398 – 1468) was a German goldsmith and publisher who introduced printing to Europe. His introduction of mechanical movable type printing to Europe started the Printing Revolution and is widely regarded as the most important event of the modern period."
+    prompt="summarize"
+    max_length=512
+    min_length=5
+
+    summaryText=summarize_with_length(prompt,text,max_length,min_length)
+    print(summaryText)
+
+
+test_code()
+
+
+
